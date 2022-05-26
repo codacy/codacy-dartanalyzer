eeb239a734be0efa294a74074aea87edb1e9313b
rubensantoscodacy <ruben.santos@codacy.com>
2022-05-26 15:11:18 +0100

    fix: revert to machine format

--- src/main/scala/codacy/dartanalyzer/model/Location.scala
+++
@@ -1,9 +0,0 @@
-package codacy.dartanalyzer.model
-
-import play.api.libs.json.Json
-
-case class Location(file: String, range: Range)
-
-object Location {
-  implicit val reads = Json.reads[Location]
-}
--- src/main/scala/codacy/dartanalyzer/model/Range.scala
+++
@@ -1,9 +0,0 @@
-package codacy.dartanalyzer.model
-
-import play.api.libs.json.Json
-
-case class Range(start: Position, end: Position)
-
-object Range {
-  implicit val reads = Json.reads[Range]
-}
